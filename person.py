3b8a7e94678af18bcd47bea7f1701e726b175241
Joanna K <134522101+Ivorwena@users.noreply.github.com>
2023-06-13 13:57:05 +0200

    Create person.py

---
+++ person.py
@@ -0,0 +1,5 @@
+class Person:
+    """This is a class implementation, class Person"""
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
